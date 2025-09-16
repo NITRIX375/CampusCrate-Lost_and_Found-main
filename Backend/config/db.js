349b533a88ee8e2eac9c4244eef1c85f1bb65db5
NITRIX375 <ajay471aj@gmail.com>
2025-09-17 02:07:06 +0530

    changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully. 🍃');
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
